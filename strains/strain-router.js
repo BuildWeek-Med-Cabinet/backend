483138c57081a4e6d46c0726742bf8ce63fd8fa2
emilio <ediaz-goico@me.bergen.edu>
2020-06-24 22:51:45 -0400

    fixed the end points

--- strains/strain-router.js
+++ strains/strain-router.js
@@ -1,43 +1,34 @@
 const express = require('express')
 
-const Stains = require('./strain-model')
+const Strains = require('./strain-model')
 const authentication = require('../middleware/restricted-middleware')
 
 
 const router = express.Router()
 
 router.get('/', authentication, async (req, res) => {
-    const {id} = req.decodedJWT
+
     try {
-        const results = await Stains.getPreferrences(id)
+        const results =  await Strains.find()
+        console.log(results)
         if (results) { return res.status(200).json(results)}
-        res.status(404).json({ message: '0 results' })
+        else {  
+         res.status(404).json({ message: '0 results' })
+        }
     } catch(e) {
         res.status(500).json({ message: 'There was an a request error' })
     }
 })
 
 router.post('/', authentication, async (req, res) => {
-    const body = req.body
-    const user = req.decodedJWT
+    const newStrain = req.body
     try {
-        const newStrain = await Stains.find(body.strain).first()
-        if (newStrain) {
-            const newPref = await Stains.checkIfInPreferrences(newStrain.id)
-            if (newPref) {
-                return res.status(400).json({ message: 'User already added new strain!' })
-            }
-            const results = await Stains.addPreferrences(user.id, newStrain.id)
-            if (results) { 
-                return res.status(200).json({ message: 'successfully added new strain!'})
-            }
+        const newMarijuana = await Strains.add(newStrain)
+        if(newMarijuana) { 
+            res.status(200).json(newMarijuana)
         } else {
-            const cannabis_id = await Stains.add(body)
-            const results = await Stains.addPreferrences(user.id, cannabis_id[0])
-            if (results) {
-                return res.status(200).json({ message: 'successfully added new strain!'})}
-            }
-        res.status(400).json({ message: 'Error adding new strain' })
+            res.status(404).json({message: 'Invalid Request'})
+        }
     } catch(e) {
         res.status(500).json({ message: 'There was a request error' })
     }
@@ -45,9 +36,9 @@ router.post('/', authentication, async (req, res) => {
 
 router.delete('/:id', authentication, async (req, res) => {
     const cannabis_id = req.params.id
-    const user_id = req.decodedJWT.id
+    const user_id = req.decodedToken.id
     try {
-        const remove = await Stains.remove(cannabis_id, user_id)
+        const remove = await Strains.remove(cannabis_id, user_id)
         if (remove > 0) { return res.status(200).json({ message: 'Strain has been removed successfully' })}
         res.status(404).json({ message: 'Strain not found' })
     } catch(e) {
